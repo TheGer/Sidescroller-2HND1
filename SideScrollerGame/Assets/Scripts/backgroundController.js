84864c1f57e2a6455fed69427775a41fdd81e928
Gerry Said <gerrysaid@gmail.com>
2014-03-03 09:56:20 +0100

    Implementation of scrolling background
    Implemented an infinitely scrolling background.

---
+++ SideScrollerGame/Assets/Scripts/backgroundController.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function OnBecameInvisible()
+{
+ //as soon as the background becomes invisible, place it on the other side of the 
+ //screen.
+ transform.position.x = Camera.main.ScreenToWorldPoint(Vector3(Screen.width,0,0)).x;
+}
+
+
+function Start () {
+
+}
+
+function Update () {
+
+}
---
+++ SideScrollerGame/Assets/Scripts/cameraController.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	//this line is used to move the camera forward at a speed of 10
+	transform.Translate(Vector3.right * 10 * Time.deltaTime);
+}
